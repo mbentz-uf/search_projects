f371f6ad425af4dbd9b14fe9d5bca275fd1ac5a2
mbentz <mbentz@ufl.edu>
2020-12-21 16:58:43 -0500

    Renames functionBus to handler

--- handler.php
+++ handler.php
@@ -5,14 +5,25 @@ require_once(__DIR__ . '/ExternalModule.php');
 
 $function = $_REQUEST["function"];
 $data = $_REQUEST["data"];
+// This needs to be within the context but does not need to be passed for self::checkProjectContext(__METHOD__); to pass
+$pid = $_REQUEST["pid"];
 
 // TODO: Figure out how to get a hold of the current ExternalModule
 $EM = new ExternalModule();
 
 // TODO: Figure out how to do this without a switch statement
 switch ($function) {
-    case 'sampleFunction':
-        $EM->sampleFunction($data);
+    case 'getExternalModules':
+        $EM->getExternalModules($data); //, $pid);
+        break;
+    case 'getProjects':
+        $EM->getProjects($data); //, $pid);
+        break;
+    case 'getUsers':
+        $EM->getUsers($data); //, $pid);
+        break;
+    case 'getAllData':
+        $EM->getAllData($data);
         break;
 }
 
